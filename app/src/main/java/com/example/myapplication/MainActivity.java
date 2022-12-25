71e60b4fc3cd1a1dc07f31ec55acd5df044ec627
TheSword123 <merm123@yandex.ru>
2022-12-25 11:03:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView resultTextView;
+    private EditText number_field_1, number_field_2;
+    private Button add_button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        resultTextView = findViewById(R.id.textView);
+
+
+    }
+}
